efbfb31f497897a9d363da138147cfe78c911e25
unknown <valerijmanukyan.manukyan@yandex.ru>
2022-11-08 15:54:16 +0300

    add new file

---
+++ django_d5.9/NewsPortal/news/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostList(ListView):
+    model = Post
+    ordering = 'title'
+    template_name = 'posts.html'
+    context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = Post
+    template_name = 'post.html'
+    context_object_name = 'post'
